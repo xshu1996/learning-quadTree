e96e4ce710002bae9cbb8fd887631bb31b345ef8
xshu <825947170@qq.com>
2021-07-25 16:41:10 +0800

    feat: 上传build文件

---
+++ web-mobile/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","ui"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/quad-tree.fire",orientation:"landscape",jsList:[]};
